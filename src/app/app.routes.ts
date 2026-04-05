5fd34a75259f95e38d757802eb52d0310c590753
TobarZS <toobarsebasstian@gmail.com>
2026-04-05 01:06:15 -0500

    App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Workspace } from './workspace/workspace';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'workspace',
+        component: Workspace,
+    },
+    {
+        path: '**',
+        component: Home,
+    },
+];
---
+++ src/app/workspace/workspace.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace',
+  templateUrl: './workspace.html',
+  styleUrl: './workspace.css',
+})
+export class Workspace {}
